8be78b96d48fc36c35218a19563778d473790fd4
Yohombu Abeysinghe <yohombuda@gmail.com>
2021-10-18 12:28:29 +0530

    dis sec uis

---
+++ public/Views/DistrictSecretariat/includes/sidebar_reports.php
@@ -0,0 +1,28 @@
+<div class="sidebar">
+    <div class="logo-details">
+        <a href="/<?php echo baseUrl; ?>">
+            <img src="/<?php echo baseUrl; ?>/public/assets/img/social-care3.png" width="" height="35" alt="logo">
+        </a>
+        <span class="logo_name">SAHANADARA</span>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="/<?php echo baseUrl; ?>/DistrictSecretariat/Report/IncidentReport" id="incident">
+                <i class='bx bxs-report' ></i>
+                <span class="links_name">Incident </span>
+            </a>
+        </li>
+        <li>
+            <a href="/<?php echo baseUrl; ?>/DistrictSecretariat/Report/SafeHouseReport" id="safe">
+                <i class='bx bxs-report' ></i>
+                <span class="links_name">Safe House </span>
+            </a>
+        </li>
+        <li>
+            <a href="/<?php echo baseUrl; ?>/DistrictSecretariat/Report/CompensationReport" id="compensation">
+                <i class='bx bxs-report' ></i>
+                <span class="links_name">Compensation </span>
+            </a>
+        </li>
+    </ul>
+</div>

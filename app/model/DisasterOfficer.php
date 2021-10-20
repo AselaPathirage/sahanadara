b5f73a50bd53dbed9f8f53c86532230e0e0f8c5a
Yohombu Abeysinghe <yohombuda@gmail.com>
2021-10-21 02:50:58 +0530

    safe house crud - add

--- app/model/DisasterOfficer.php
+++ app/model/DisasterOfficer.php
@@ -2,4 +2,54 @@
 class DisasterOfficer extends Noticer{
     use Viewer;
     use Alerter;
+
+    public function __construct($con){
+        parent::__construct($con);
+    }
+
+    public function addSafehouse(array $data){
+        global $errorCode;
+        // print_r($data);exit;
+        $safeHouseAddress = $data['safeHouseAddress'];
+        $safeHouseName = $data['safeHouseName'];
+        $safeHouseTelno = $data['safeHouseTelno'];
+        $gndvId =$data['gnDiv'];
+        $sql = "INSERT INTO `safehouse` (`safeHouseAddress`, `safeHouseName`) VALUES ('$safeHouseAddress', '$safeHouseName');";
+        $this->connection->query($sql);
+        $result = array();
+        $sql = "SELECT LAST_INSERT_ID();";
+        $execute = $this->connection->query($sql);
+        $r = $execute-> fetch_assoc();
+        $safeHouseID = $r["LAST_INSERT_ID()"];
+        $sql = "INSERT INTO `safehousecontact` (`safeHouseID`, `safeHouseTelno`) VALUES ('$safeHouseID', '$safeHouseTelno');";
+        $this->connection->query($sql);
+        $sql = "UPDATE gndivision
+        SET safeHouseID = $safeHouseID
+        WHERE gndvId = $gndvId;";
+        $this->connection->query($sql);
+        echo json_encode(array("code"=>$errorCode['success']));
+    }
+    public function getGNDivision(array $data){
+        $id = $data['id'];
+        $sql = "SELECT g.* FROM gndivision g,divisionaloffice d,dismgtofficer m 
+        WHERE m.disMgtOfficerID = d.disasterManager AND g.dvId = d.dvId AND m.disMgtOfficerID = $id  AND g.safeHouseID IS NULL";
+        $excute = $this->connection->query($sql);
+        $results = array();
+        while($r = $excute-> fetch_assoc()) {
+            $results[] = $r;
+        }
+        $json = json_encode($results);
+        echo $json;
+    }
+    public function getUnit(){
+        $sql = "SELECT * FROM `unit` ORDER BY unitName";
+        $excute = $this->connection->query($sql);
+        $results = array();
+        while($r = $excute-> fetch_assoc()) {
+            $results[] = $r;
+        }
+        $json = json_encode($results);
+        echo $json;
+    }
+        
 }

b1a44c74709484304d5673a9e1c36c5a9de26716
AselaPathirage <aselapathirage@gmail.com>
2021-10-24 12:21:23 +0530

    GN uis modified

--- public/Views/GramaNiladari/index.php
+++ public/Views/GramaNiladari/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="/<?php echo baseUrl; ?>/public/assets/css/dashboard.css">
     <link rel="stylesheet" href="/<?php echo baseUrl; ?>/public/assets/css/dashboard_component.css">
     <link rel="stylesheet" href="/<?php echo baseUrl; ?>/public/assets/css/style_dmc.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Boxicons -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
@@ -27,7 +28,7 @@
         <div class="container">
             <h1 class="text-center">Welcome Grama Niladari</h1>
             <div class="space"></div>
-            <div class="stat-row">
+            <!-- <div class="stat-row">
                 <div class="box">
                     <div class="right-side">
                         <div class="box-topic">Approved Compensation Requests</div>
@@ -53,25 +54,94 @@
                     <i class='bx bx-building-house cart three'></i>
                 </div>
 
-            </div>
+            </div> -->
+
+            <section class="services">
+                <a href="">
+                    <div class="services__box">
+                        <figure class="services__icon" style="--i:#ffb508">
+                            <ion-icon name="videocam-outline">
+                                <i class='bx bxs-report '></i>
+                                <!-- <i class="fas fa-hands-helping"></i> -->
+                            </ion-icon>
+                            <h2 class="services__title" style="color: black; font-size: 52px; font-weight: bold;">
+                                40
+                            </h2>
+                        </figure>
+                        <div class="services__content" style="margin-top: 130px;">
+                            <h2 class="services__title">
+                                Approved Compensation Requests
+                            </h2>
+                            <!-- <p class="services__description">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ipsum nemo. Vel consequuntur ratione laborum.
+                                        </p> -->
+                        </div>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="services__box">
+                        <figure class="services__icon" style="--i:#4eb7ff">
+                            <ion-icon name="videocam-outline">
+                                <i class='bx bxs-report '></i>
+                                <!-- <i class="fas fa-hands-helping"></i> -->
+                            </ion-icon>
+                            <h2 class="services__title" style="color: black; font-size: 52px; font-weight: bold;">
+                                40
+                            </h2>
+                        </figure>
+                        <div class="services__content" style="margin-top: 130px;">
+                            <h2 class="services__title">
+                                Approved Incident <br> Reports
+                            </h2>
+                            <!-- <p class="services__description">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ipsum nemo. Vel consequuntur ratione laborum.
+                                        </p> -->
+                        </div>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="services__box">
+                        <figure class="services__icon" style="--i:#fd6494">
+                            <ion-icon name="videocam-outline">
+                                <i class='bx bx-building-house '></i>
+                                <!-- <i class="fas fa-hands-helping"></i> -->
+                            </ion-icon>
+                            <h2 class="services__title" style="color: black; font-size: 52px; font-weight: bold;">
+                                140
+                            </h2>
+                        </figure>
+                        <div class="services__content" style="margin-top: 160px;">
+                            <h2 class="services__title">
+                                Residents
+                            </h2>
+                            <!-- <p class="services__description">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ipsum nemo. Vel consequuntur ratione laborum.
+                                        </p> -->
+                        </div>
+                    </div>
+                </a>
+
+            </section>
+
+
+
+
         </div>
         <div class="space"></div>
         <div class="container text-center">
-            <div class="row-content alert-div" style="margin: 10px auto;">
+            <div class="row-content alert-div alert-warning" style="margin: 10px auto;">
+                <button type="button" class="close-alert">×</button>
                 <p>Rainfall over 150mm recorded in catchment areas Aththanagalu Oya Basin. High risk of
                     of minor flooding in low lying areas. Area residents requested to be alert. DMC 117
                 </p>
             </div>
             <div class="row-content alert-div" style="margin: 10px auto;">
+                <button type="button" class="close-alert">×</button>
                 <p>Rainfall over 150mm recorded in catchment areas Aththanagalu Oya Basin. High risk of
                     of minor flooding in low lying areas. Area residents requested to be alert. DMC 117
                 </p>
             </div>
-            <div class="row-content alert-div" style="margin: 10px auto;">
-                <p>Rainfall over 150mm recorded in catchment areas Aththanagalu Oya Basin. High risk of
-                    of minor flooding in low lying areas. Area residents requested to be alert. DMC 117
-                </p>
-            </div>
+
 
         </div>
     </section>
@@ -92,6 +162,11 @@
         sidebarBtn.onclick = function() {
             sidebar.classList.toggle("active");
         }
+
+        $(".close-alert").click(function(e) {
+            $(this).parent().remove();
+            e.preventDefault();
+        });
     </script>
 </body>
 

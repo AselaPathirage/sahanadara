092aebcb6052c0fe968bff8f186e24244723f935
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-12-20 13:06:09 +0530

    google/cloud-translate added

--- api/libraries/vendor/composer/autoload_real.php
+++ api/libraries/vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit3b82dacf2a6b2cead030f3abee59d923
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit3b82dacf2a6b2cead030f3abee59d923::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3b82dacf2a6b2cead030f3abee59d923($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire3b82dacf2a6b2cead030f3abee59d923($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }

7bce66dc239d30f1c816fbe83235d959dee340c3
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-12-23 13:06:39 +0530

    viewver backend

--- api/model/SafeHouse.php
+++ api/model/SafeHouse.php
@@ -1,4 +1,16 @@
 <?php
 class SafeHouse{
-    
+    public static function getId($input){
+        $input =strtoupper($input);
+        $id = explode("SH",$input);
+        if(count($id)==2){
+            return (int)$id[1];
+        }
+        return (int)$id[0];
+    }
+    public static function getItemCode($id){
+        $numlength = strlen((string)$id);
+        $code = "SH".str_repeat("0",5-$numlength).$id;
+        return $code;
+    }
 }

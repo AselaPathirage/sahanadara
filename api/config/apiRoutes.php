b2454b7b2682b3553bd5d7f8dbe3387d4ddc8fca
AselaPathirage <aselapathirage@gmail.com>
2022-01-07 20:50:35 +0530

    routes added

--- api/config/apiRoutes.php
+++ api/config/apiRoutes.php
@@ -7,34 +7,36 @@ DELETE - delete
 */
 
 
-Route::GET("donation",array("Home@viewDonations"));
-Route::GET("notice",array("Home@viewNotice"));
-Route::GET("role",array("Employee@getRole"));
-Route::GET("division",array("Admin@getDivision","InventoryManager@getDvOfficeList"));
-Route::GET("GnDivision",array("Admin@getGnDivision","DisasterOfficer@getGNDivision","InventoryManager@getGNDivision"));
-Route::GET("item",array("InventoryManager@getItem","ResponsiblePerson@getItem"));
-Route::GET("safehouse",array("DisasterOfficer@viewSafehouse","InventoryManager@getSafeHouseAll","DivisionalSecretariat@getSafeHouseAll","Dmc@getSafeHouseAll")); 
-Route::GET("district",array("Admin@getDistrict"));
-Route::GET("user",array("InventoryManager@getMySelf","Admin@searchUser")); //user/userId/required data
-Route::GET("report",array("Employee@report"));
+Route::GET("donation", array("Home@viewDonations"));
+Route::GET("notice", array("Home@viewNotice"));
+Route::GET("role", array("Employee@getRole"));
+Route::GET("division", array("Admin@getDivision", "InventoryManager@getDvOfficeList"));
+Route::GET("GnDivision", array("Admin@getGnDivision", "DisasterOfficer@getGNDivision", "InventoryManager@getGNDivision"));
+Route::GET("item", array("InventoryManager@getItem", "ResponsiblePerson@getItem"));
+Route::GET("safehouse", array("DisasterOfficer@viewSafehouse", "InventoryManager@getSafeHouseAll", "DivisionalSecretariat@getSafeHouseAll", "Dmc@getSafeHouseAll"));
+Route::GET("district", array("Admin@getDistrict"));
+Route::GET("user", array("InventoryManager@getMySelf", "Admin@searchUser")); //user/userId/required data
+Route::GET("report", array("Employee@report"));
+Route::GET("profile", array("GramaNiladari@getProfileDetails"));
 
-Route::PUT("resetPassword",array("Employee@updatePassword"));
+Route::PUT("resetPassword", array("Employee@updatePassword"));
+Route::PUT("profile", array("GramaNiladari@updateProfileDetails"));
 
-Route::POST("login",array("Employee@login"));
-Route::POST("resetPassword",array("Employee@resetPassword"));
-Route::POST("renew",array("Employee@renew"));
-Route::POST("rewoke",array("Employee@rewoke"));
-Route::POST("user",array("Admin@register","DivisionalSecretariat@register","DisasterOfficer@register"));
-Route::POST("notice",array("InventoryManager@addNotice"));
+Route::POST("login", array("Employee@login"));
+Route::POST("resetPassword", array("Employee@resetPassword"));
+Route::POST("renew", array("Employee@renew"));
+Route::POST("rewoke", array("Employee@rewoke"));
+Route::POST("user", array("Admin@register", "DivisionalSecretariat@register", "DisasterOfficer@register"));
+Route::POST("notice", array("InventoryManager@addNotice"));
 
 // Grama Niladari
 Route::GET("residents", array("GramaNiladari@getResident"));
-Route::GET("gnprofile", array("GramaNiladari@getProfileDetails"));
 Route::GET("gnmsg", array("GramaNiladari@getMessages"));
+Route::GET("gnalert", array("GramaNiladari@getAlerts"));
+Route::GET("residentcount", array("GramaNiladari@getResidentCount"));
 Route::POST("residents", array("GramaNiladari@addResident"));
 Route::POST("gnmsg", array("GramaNiladari@sendMessages"));
 Route::PUT("residents", array("GramaNiladari@updateResident"));
-Route::PUT("gnprofile", array("GramaNiladari@updateProfileDetails"));
 Route::DELETE("residents", array("GramaNiladari@deleteResident"));
 
 // Disaster Mgt officer

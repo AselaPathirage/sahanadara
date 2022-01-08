6974d815c0fcad61f62c0c6d7a613c6f5d98102c
yohombu <yohombuda@gmail.com>
2022-01-08 18:32:12 +0530

    safehouse crud

--- public/Views/DisasterOfficer/SafeHouse/addsafehouse.php
+++ public/Views/DisasterOfficer/SafeHouse/addsafehouse.php
@@ -33,7 +33,7 @@
                     <a class="btn_blue Click-here">Add Safe House</a>
                 </div>
             </div>
-            <div class="custom-model-main">
+            <div class="custom-model-main addform">
                 <div class="custom-model-inner">
                     <div class="close-btn">×</div>
                     <div class="custom-model-wrap">
@@ -57,7 +57,7 @@
                                                 </select>
 
                                 <label for="safeHouseTelno">Tele Number</label>
-                                <input type="text" id="safeHouseTelno" name="safeHouseTelno" maxlength="10" required='true'>
+                                <input type="text" id="safeHouseTelno" name="safeHouseTelno" maxlength="10" onkeypress="return isNumber(event)" required='true' />
                                     <div class="row" style="justify-content: center;">
                                         <input type="submit" value="Submit" id="submit" class="btn-alerts" />
                                         <input type="reset" value="Reset" class="btn-alerts" />
@@ -69,9 +69,64 @@
                 </div>
                 <div class="bg-overlay"></div>
             </div>
+            <div class="custom-model-main" id="updateform">
+                <div class="custom-model-inner">
+                    <div class="close-btn">×</div>
+                    <div class="custom-model-wrap">
+                        <div class="pop-up-content-wrap">
+                            <form action="#" id='update' name="update" method="POST">
+                                <h1>Update Safe House</h1>
+
+                                <div class="row-content">
+
+                                <label for="safeHouseAddress">Safe House Address</label>
+                                <textarea id="upsafeHouseAddress" name="upsafeHouseAddress" required='true'></textarea>
+                                <!-- <input type="hidden" id="item" value=""> -->
+                                <label for="safeHouseName">Name</label>
+                                <input type="text" id="upsafeHouseName" name="upsafeHouseName" required='true'>
+
+                                <label for="gnDiv">Grama Niladhari Division</label>
+                                <!-- <input type="text" id="gnDiv" name="gnDiv"> -->
+
+                                <select id="upgnDiv" name="upgnDiv">
+                                                    <option>Select GN Division</option>
+                                                </select>
+
+                                <label for="safeHouseTelno">Tele Number</label>
+                                <input type="text" id="upsafeHouseTelno" name="upsafeHouseTelno" maxlength="10" onkeypress="return isNumber(event)" required='true' />
+                                    <div class="row" style="justify-content: center;">
+                                        <input type="submit" value="Update" class="btn-alerts" />
+                                        <input type="reset" value="Reset" class="btn-alerts" />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-overlay"></div>
+            </div>
+            <div class="custom-model-main" id="deleteform">
+                <div class="custom-model-inner">
+                    <div class="close-btn">×</div>
+                    <div class="custom-model-wrap">
+                        <div class="pop-up-content-wrap">
+                            <div class="row-content">
+                                <h2>Are you sure?</h2>
+                                <input type="hidden" id="item2" value="">
+                                <p>Do you really want to delete this record? This process cannot be undone.</p>
+                                <div class="row" style="justify-content: center;">
+                                    <button type="button" class="btn-alerts btn_cancel cancel">Cancel</button>
+                                    <button type="button" class="btn-alerts btn_danger" id="delete-confirm">Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-overlay"></div>
+            </div>
             <div class="container">
                 <div class="">
-                    <!-- <table class="table" style="margin: 15px auto;">
+                    <table class="table" style="margin: 15px auto;">
                         <thead>
                             <tr class="filters">
 
@@ -81,11 +136,11 @@
                                 </th>
                             </tr>
                         </thead>
-                    </table> -->
+                    </table>
 
 
                     <div class="panel panel-primary filterable">
-                        <table id="" class="table">
+                        <table id="table2" class="table">
                             <thead>
                                 <tr>
                                     <th>Address</th>
@@ -130,9 +185,93 @@
         // getUnit();
             // getItem();
             viewSafehouse();
+
+                $(".btn_update").on('click', function() {
+                    var id = $(this).attr("id");
+                    var safeHouseAddress = $(this).attr("data-safeHouseAddress");
+                    var safeHouseName = $(this).attr("data-safeHouseName");
+                    var safeHouseTelno = $(this).attr("data-safeHouseTelno");
+                    var gnDiv = $(this).attr("data-gnDiv");
+
+                    $("#updateform").fadeIn();
+                    $("#update").trigger("reset");
+                    $("#updateform").addClass('model-open');
+
+                    $("#upsafeHouseAddress").val(safeHouseAddress);
+                    $("#upsafeHouseName").val(safeHouseName);
+                    $("#upsafeHouseTelno").val(safeHouseTelno);
+                    $("#upgnDiv").val(gnDiv);
+                    $("#item").val(id);
+                });
+                $(".btn_delete").on('click', function() {
+                    var id = $(this).attr("id");
+                    // var nic = $(this).attr("data-nic");
+                    // var name = $(this).attr("data-name");
+                    // var address = $(this).attr("data-address");
+                    // var telno = $(this).attr("data-telno");
+
+                    $("#deleteform").fadeIn();
+
+                    $("#deleteform").addClass('model-open');
+
+                    // $("#upname").val(name);
+                    // $("#upnic").val(nic);
+                    // $("#upphone").val(telno);
+                    // $("#upaddress").val(address);
+                    $("#item2").val(id);
+                });
+            //validations
+            function isNumber(e) {
+            e = (e) ? e : window.event;
+            var charCode = (e.which) ? e.which : e.keyCode;
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+            }
+                return true;
+            }
+            
+            function alertGen($messege, $type) {
+                if ($type == 1) {
+                    $("#alertBox").html("  <div class='alert success-alert'><h3>" + $messege + "</h3><a id='closeMessege' class='closeMessege'>&times;</a></div>");
+                    setTimeout(function() {
+                        $(".alert").fadeOut(100)
+                        $("#alertBox").html("");
+                    }, 4000);
+                } else if ($type == 2) {
+                    $("#alertBox").html("  <div class='alert warning-alert'><h3>" + $messege + "</h3><a id='closeMessege' class='closeMessege'>&times;</a></div>");
+                    setTimeout(function() {
+                        $(".alert").fadeOut(100)
+                        $("#alertBox").html("");
+                    }, 4000);
+                } else {
+                    $("#alertBox").html("  <div class='alert danger-alert'><h3>" + $messege + "</h3><a id='closeMessege' class='closeMessege'>&times;</a></div>");
+                    setTimeout(function() {
+                        $(".alert").fadeOut(100)
+                        $("#alertBox").html("");
+                    }, 4000);
+                }
+            }
+
+            let sidebar = document.querySelector(".sidebar");
+            let sidebarBtn = document.querySelector(".sidebarBtn");
+            sidebarBtn.onclick = function() {
+                sidebar.classList.toggle("active");    
+            }
+            
+            // popup
+            $(".Click-here").on('click', function() {
+            $(".addform").fadeIn();
+            $("#add").trigger("reset");
+            $(".addform").addClass('model-open');
+            });
+            $(".close-btn, .bg-overlay, .cancel").click(function() {
+                $(".custom-model-main").removeClass('model-open');
+            });
+
             $("#add").submit(function(e) {
                 e.preventDefault();
-                //$(".custom-model-main").removeClass('model-open');
+                $(".custom-model-main").fadeOut();
+                $(".custom-model-main").removeClass('model-open');
                 var str = [];
                 var formElement = document.querySelector("#add");
                 var formData = new FormData(formElement);
@@ -152,20 +291,99 @@
 						// window.location.replace("<?php echo HOST ?>DisasterOfficer/SafeHouse/SafeHouseDetails");
                         $('#tableSafeHouse').empty();
                         viewSafehouse();
-					},
-					error: function(err) {
-						alert(err);
-					}
-				});
+                        console.log(result.code);
+                        if (result.code == 806) {
+                            alertGen(" Record Added Successfully!", 1);
 
+                        } else {
+                            alertGen(" Unable to handle request.", 2);
+                        }
+                    },
+                    error: function(err) {
+                        alertGen(" Something went wrong.", 3);
+                        console.log(err);
+                    }
+                });
             });
-		
 
-        let sidebar = document.querySelector(".sidebar");
-        let sidebarBtn = document.querySelector(".sidebarBtn");
-        sidebarBtn.onclick = function() {
-            sidebar.classList.toggle("active");
-        }
+            $("#update").submit(function(e) {
+            e.preventDefault();
+            $("#updateform").fadeOut();
+            $("#updateform").removeClass('model-open');
+            var str = [];
+            var object = {};
+            var formElement = document.querySelector("#update");
+            var formData = new FormData(formElement);
+            //var array = {'key':'ABCD'}
+            var object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+            $("#update").trigger('reset');
+            var json = JSON.stringify(object);
+            var id = document.getElementById("item").value;
+            console.log(json);
+            $.ajax({
+                type: "PUT",
+                url: "<?php echo API; ?>safehouse/" + id,
+                data: json,
+                headers: {
+                    'HTTP_APIKEY': '<?php echo $_SESSION['key'] ?>'
+                },
+                cache: false,
+                success: function(result) {
+                    $('#trow').empty();
+                    viewSafehouse();
+                    location.reload();
+                    if (result.code == 806) {
+                        alertGen("Record Updated Successfully!", 1);
+                    } else {
+                        alertGen("Unable to handle request.", 2);
+                    }
+                },
+                error: function(err) {
+                    alertGen(" Something went wrong.", 3);
+                    console.log(err);
+                }
+            });
+        });
+        $("#delete-confirm").on("click", function(e) {
+            e.preventDefault();
+            $("#deleteform").fadeOut();
+            $("#deleteform").removeClass('model-open');
+            var str = [];
+            //var array = {'key':'ABCD'}
+            var object = {};
+            var id = document.getElementById("item2").value;
+            object["id"] = id;
+            var json = JSON.stringify(object);
+            console.log(json);
+            $.ajax({
+                type: "DELETE",
+                url: "<?php echo API; ?>safehouse/" + id,
+                data: json,
+                headers: {
+                    'HTTP_APIKEY': '<?php echo $_SESSION['key'] ?>'
+                },
+                cache: false,
+                success: function(result) {
+                    $('#trow').empty();
+                    viewSafehouse();
+                    location.reload();
+                    if (result.code == 806) {
+                        alertGen("Record Updated Successfully!", 1);
+                    } else {
+                        alertGen("Unable to handle request.", 2);
+                    }
+                },
+                error: function(err) {
+                    alertGen("Something went wrong.", 3);
+                    console.log(err);
+                }
+            });
+        });
+
+        
 
         function getGNDivision(){
             output = $.parseJSON($.ajax({
@@ -187,13 +405,7 @@
             }
         }
         getGNDivision();
-        // popup
-        $(".Click-here").on('click', function() {
-            $(".custom-model-main").addClass('model-open');
-        });
-        $(".close-btn, .bg-overlay").click(function() {
-            $(".custom-model-main").removeClass('model-open');
-        });
+        
 
         function viewSafehouse(){
             output = $.parseJSON($.ajax({
@@ -204,10 +416,13 @@
                 cache: false,
                 async: false
             }).responseText);
+            //console.log(output);
+            $("#tableSafeHouse").empty();
             var table = document.getElementById("tableSafeHouse");
+            
             for (var i = 0; i < output.length; i++){
                 let obj = output[i];
-                console.log(obj);
+                //console.log(obj);
                 // let row = table.insertRow(-1);
                 // let id ="data_"+i;
                 // row.id = id;
@@ -228,16 +443,22 @@
                 let cell3 = row.insertCell(-1);
                 let cell4 = row.insertCell(-1);
                 let cell5 = row.insertCell(-1);
+
                 var attribute = document.createElement("a");
-                attribute.id = i;
+                attribute.id = obj['safeHouseID'];
                 attribute.href = "";
-                attribute.className = "btn_blue";
+                attribute.className = "btn_update btn_blue";
                 attribute.name = "update";
                 attribute.innerHTML = "Update";
+                attribute.setAttribute("data-safeHouseAddress", obj['safeHouseAddress'])
+                attribute.setAttribute("data-safeHouseName", obj['safeHouseName'])
+                attribute.setAttribute("data-safeHouseTelno", obj['safeHouseTelno'])
+                attribute.setAttribute("data-gnDiv", obj['gnDiv'])
                 var attribute2 = document.createElement("a");
+                
 
-                attribute2.id = i;
-                attribute2.href = "";
+                attribute2.id = obj['safeHouseID'];
+                //attribute2.href = "";
                 attribute2.className = "btn_delete";
                 attribute2.name = "delete";
                 attribute2.innerHTML = "Delete";
@@ -255,8 +476,25 @@
                 
             }
         }
+        // search
 
-
+        (function() {
+            var showResults;
+            $('#search').keyup(function() {
+                var searchText;
+                searchText = $('#search').val();
+                return showResults(searchText);
+            });
+            showResults = function(searchText) {
+                $('tbody tr').hide();
+                return $('tbody tr:Contains(' + searchText + ')').show();
+            };
+            jQuery.expr[':'].Contains = jQuery.expr.createPseudo(function(arg) {
+                return function(elem) {
+                    return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+                };
+            });
+        }.call(this));
     </script>
 </body>
 </html>

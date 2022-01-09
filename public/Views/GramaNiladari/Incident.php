92588ea666651d774e6a3ebd800700a2d417023d
AselaPathirage <aselapathirage@gmail.com>
2022-01-09 20:01:01 +0530

    inc added

--- public/Views/GramaNiladari/Incident.php
+++ public/Views/GramaNiladari/Incident.php
@@ -49,8 +49,19 @@
             </div>
 
 
-            <div class="container">
+            <div class="container" id="tbodyid">
                 <div class="row">
+                    <div class="col6">
+                        <div class="box row-content" style="position:relative;">
+                            <h4>Flood in Kadana</h4>
+                            <p>Affected the valleys of river Kalu </p>
+
+                            <div class="row" style="text-align: right; margin: 0 auto;display:block;">
+                                <a class="btn_active" style="position: absolute; top:15px;right:35px;">Status : Active</a>
+                                <a href="/<?php echo baseUrl; ?>/GramaNiladari/IncidentView" class="btn_views">View</a>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col6">
                         <div class="box row-content">
                             <h4>Flood in Kadana</h4>
@@ -61,17 +72,12 @@
                                 <a href="/<?php echo baseUrl; ?>/GramaNiladari/IncidentView" class="btn_views">View</a>
                             </div>
                         </div>
-                        <div class="box row-content">
-                            <h4>Flood in Kadana</h4>
-                            <p>Affected the valleys of river Kalu </p>
-
-                            <div class="row" style="text-align: right; margin: 0 auto;display:block">
-                                <a href="/<?php echo baseUrl; ?>/GramaNiladari/IncidentView" class="btn_views">View</a>
-                            </div>
-                        </div>
-
                     </div>
-                    <div class="col6" style="overflow: auto">
+
+
+
+
+                    <!-- <div class="col6" style="overflow: auto">
                         <div class="box row-content">
                             <h4>Flood in Kadana</h4>
                             <p>Affected the valleys of river Kalu </p>
@@ -89,7 +95,7 @@
                                 <a href="/<?php echo baseUrl; ?>/GramaNiladari/IncidentView" class="btn_views">View</a>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
 
             </div>
@@ -106,12 +112,57 @@
                 $(thisPage).addClass("active");
             });
 
+            getIncidents();
+
         });
 
         let sidebar = document.querySelector(".sidebar");
         let sidebarBtn = document.querySelector(".sidebarBtn");
         sidebarBtn.onclick = function() {
             sidebar.classList.toggle("active");
+        }
+
+        function getIncidents() {
+            // var object = {};
+
+
+            // var json = JSON.stringify(object);
+            // console.log(object);
+            output = $.parseJSON($.ajax({
+                type: "GET",
+                url: "<?php echo API; ?>incident",
+                dataType: "json",
+                headers: {
+                    'HTTP_APIKEY': '<?php echo $_SESSION['key'] ?>'
+                },
+                cache: false,
+                async: false
+            }).responseText);
+            // console.log(output);
+            $("#tbodyid").empty();
+            var $sample = "";
+            if (output == null) {
+                $sample += "<p>No incident data</p>";
+            } else {
+                for (var i = 0; i < output.length; i++) {
+                    let obj = output[i];
+                    console.log(obj);
+
+                    if (i % 2 == 0) {
+                        $sample += "<div class='row'>";
+                    }
+                    $sample += "<div class='col6'><div class='box row-content' style='position:relative;'><h4>" + obj['title'] + "</h4><p>" + obj['description'] + "</p><div class='row' style='text-align: right; margin: 0 auto;display:block;'>";
+                    if (obj['isActive'] == 1) {
+                        $sample += "<a class='btn_active' style='position: absolute; top:15px;right:35px;'>Status : Active</a>";
+                    }
+                    $sample += "<a href='/<?php echo baseUrl; ?>/GramaNiladari/IncidentView/" + obj['incidentId'] + "' class='btn_views'>View</a></div></div></div>";
+                    if ((i % 2 == 1) || (i == output.length - 1)) {
+                        $sample += "</div>";
+                    }
+                }
+            }
+            console.log($sample);
+            $("#tbodyid").append($sample);
         }
     </script>
 </body>

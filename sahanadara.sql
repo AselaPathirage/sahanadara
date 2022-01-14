42c950febace14ba1f4173eb8a97973dd8c36339
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-01-15 00:29:32 +0530

    db update

--- sahanadara.sql
+++ sahanadara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2022 at 07:30 PM
+-- Generation Time: Jan 14, 2022 at 07:59 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 
@@ -1387,7 +1387,7 @@ ALTER TABLE `login`
 -- Indexes for table `noticeitem`
 --
 ALTER TABLE `noticeitem`
-  ADD PRIMARY KEY (`noticeId`);
+  ADD PRIMARY KEY (`noticeId`,`itemName`);
 
 --
 -- Indexes for table `relief`

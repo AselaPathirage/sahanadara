23fc9625aca1e40f87ed9442d79162d29bff2bc8
AselaPathirage <aselapathirage@gmail.com>
2022-01-24 02:40:05 +0530

    lang button added

--- public/Views/landing_topnav.php
+++ public/Views/landing_topnav.php
@@ -22,25 +22,50 @@
             <li class="menu-item "><a class="nav__link" href="<?php echo HOST; ?>about">About </a>
             <li class="menu-item "><a class="nav__link round" href="<?php echo HOST; ?>help">Help </a>
             <li class="menu-item "><a class="nav__link round" href="<?php echo HOST; ?>donate">Donate </a>
-            <?php
+                <?php
                 $folder = array(
-                                    5 => 'Admin',
-                                    6 => 'DisasterOfficer',
-                                    3 => 'DistrictSecretariat',
-                                    4 => 'DivisionalSecretariat',
-                                    7 => 'DMC',
-                                    1 => 'GramaNiladari',
-                                    2 => 'InventoryManager',
-                                    8 => 'ResponsiblePerson'
-                                );
-                if(isset($_SESSION['key']) && isset($_SESSION['name']) && isset($_SESSION['userRole'])){
-                    echo "<li class='menu-item'><a class='nav__link' href='".HOST.$folder[$_SESSION['userRole']]."'>Hi, ".$_SESSION['name']." </a>";
-                }else{
-                    echo "<li class='menu-item'><a class='nav__link' href='".HOST."staff'>Staff </a>";
+                    5 => 'Admin',
+                    6 => 'DisasterOfficer',
+                    3 => 'DistrictSecretariat',
+                    4 => 'DivisionalSecretariat',
+                    7 => 'DMC',
+                    1 => 'GramaNiladari',
+                    2 => 'InventoryManager',
+                    8 => 'ResponsiblePerson'
+                );
+                if (isset($_SESSION['key']) && isset($_SESSION['name']) && isset($_SESSION['userRole'])) {
+                    echo "<li class='menu-item'><a class='nav__link' href='" . HOST . $folder[$_SESSION['userRole']] . "'>Hi, " . $_SESSION['name'] . " </a>";
+                } else {
+                    echo "<li class='menu-item'><a class='nav__link' href='" . HOST . "staff'>Staff </a>";
                 }
-            ?>
+                ?>
 
             </li>
+
+            <li class="menu-item ">
+                <div style="border-left: 2px solid #e8eaed;padding:0 15px;">
+                    <label class="select" for="slct" style="margin: 0;">
+                        <select id="slct" required="required">
+
+                            <option value="#">EN</option>
+                            <option value="#">සිං</option>
+                            <option value="#">தமிழ்</option>
+
+                        </select>
+                        <!-- <svg>
+                        <use xlink:href="#select-arrow-down"></use>
+                    </svg> -->
+                    </label>
+                    <!-- SVG Sprites-->
+                    <!-- <svg class="sprites">
+                    <symbol id="select-arrow-down" viewbox="0 0 10 6">
+                        <polyline points="1 1 5 5 9 1"></polyline>
+                    </symbol>
+                </svg> -->
+
+
+                </div>
+
         </ul>
     </div>
 </nav>

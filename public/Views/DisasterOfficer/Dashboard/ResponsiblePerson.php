fd2ab612e7992368b44d3b15f0458dd09e2fdc60
Yohombu Abeysinghe <yohombuda@gmail.com>
2022-01-27 10:06:27 +0530

    responsible register

--- public/Views/DisasterOfficer/Dashboard/ResponsiblePerson.php
+++ public/Views/DisasterOfficer/Dashboard/ResponsiblePerson.php
@@ -25,78 +25,130 @@
         <!-- ======================================================================================================================================= -->
         <!-- content frome below -->
         <!-- STATS -->
-        <div class="container">
-        <table class="table">
-                    <thead>
-                        <tr class="filters">
-                            <th>Active Status
-                                <select id="status-filter" class="form-control">
-                                    <option>Active</option>
-                                    <option>Not Active</option>
-                                </select>
-                            </th>
-                            <th>GN Division
-                                <select id="milestone-filter" class="form-control">
-                                    <option>None</option>
-                                    <option>Palannoruwa</option>
-                                    <option>Koralaema</option>
-                                    <option>Olaboduwa </option>
-                                </select>
-                            </th>
-                            
-                            <th>Search
-                                <input type="text" id="search" placeholder="Search" title="Type " class="form-control">
-                            </th>
-                        </tr>
-                    </thead>
-                </table>
-
-
-                <div class="container">
-                    <div class="row">
-                        <div class="col6">
-                            <div class="box row-content">Name - Naween Pasindu<br>Office - Horana<br>NIC - 981060920V<br><br><a href="./viewres.php" class="btn-box">Active</a></div>
-                            <div class="box row-content">Name - Yohombu Abeysinghe<br>Office - Millaniya<br>NIC - 981620631V<br><br><a href="./viewres.php" class="btn-box">Active</a></div> 
-                            <div class="box row-content">Name - Asela Pathirage<br>Office - Ballapitiya<br>NIC - 991091947V<br><br><a href="./viewres.php" class="btn-box">Active</a></div> 
-                            <div class="box row-content">Name - Sanduni Rashmika<br>Office - Dodangoda<br>NIC - 987090723V<br><br><a href="./viewres.php" class="btn-box">Active</a></div>
-                            <div class="box row-content">Name - Isuri Abeysinghe<br>Office - Kesbawa<br>NIC - 947410385V<br><br><a href="./viewres.php" class="btn-box">Active</a></div>
-                           
-                        </div>
-                        <div class="col6" style="overflow: none">
-                            <div class="box row-content" style="height:100%;min-height: 300px;">
-                                <h2>Responsible Person</h1>
-                                <label for="your_name">NIC</label>
-                                <input type="text" id="nic" name="yourname" />
-
-                                <label for="f_name">First name</label>
-                                <input type="text" id="f_name" name="f_name" />
-
-                                <label for="l_name">Last Name</label>
-                                <input type="text" id="l_name" name="l_name" />
-
-                                <label for="address">Address</label>
-                                <input type="text" id="adress" name="address" />
-
-                                <label for="safehouse">Safehouse</label>
-                                <input type="text" id="safehouse" name="safehouse" />
-
-                                <label for="email">Email</label>
-                                <input type="email" id="adress" name="youremail" />
-
-                                <label for="address">Tele Number</label>
-                                <input type="text" id="tp" name="tpnum" />
-
-
-                                <div class="space"></div>
-                                <a href="" class="btn_active">Add</a>
-                                <a href="" class="btn_remove">Remove</a>
+        <div class="container col8">
+            <div class="box">
+                    <div class="box1">
+                        <form id='add' name="add" method="post" onSubmit="return validate_nic();">
+                            <h1 class="text-center">New Responsible Person</h1>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="firstname">First Name</label>
+                                </div>
+                                <div class="col9">
+                                    <input type="text" id="firstname" name="firstname" placeholder="First Name" required='true'>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="lastname">Last Name</label>
+                                </div>
+                                <div class="col9">
+                                    <input type="text" id="lastname" name="lastname" placeholder="Last Name" required='true'>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="NIC">NIC</label>
+                                </div>
+                                <div class="col9">
+                                    <div id="nicCheck">
+                                    </div>
+                                    <input type="text" id="NIC" name="NIC" placeholder="NIC" maxlength="12" required='true'>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="email">Email</label>
+                                </div>
+                                <div class="col9">
+                                    <input type="email" id="email" name="email" placeholder="Email" required='true'>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="address">Address</label>
+                                </div>
+                                <div class="col9">
+                                    <textarea type="text" id="address" name="address" placeholder="Address" required='true' ></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="TP number">TP Number</label>
+                                </div>
+                                <div class="col9">
+                                    <input type="text" id="TP_number" name="TP_number" placeholder="Phone number" maxlength="10" onkeypress="return isNumber(event)" required='true'>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col3">
+                                    <label for="user role">User role</label>
+                                </div>
+                                <div class="col9">
+                                    <select id="user_role" name="user_role" required='true'>
+                                        <option value="" selected='true'>Select</option>
+                                        <option value="1">Grama Niladhari</option>
+                                        <option value="4">Divisional Secretariat</option>
+                                        <option value="6">Disaster management officer</option>
+                                        <option value="3">District secretariat</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row" id="DistrictBox">
+                                <div class="col3">
+                                    <label for="user role">District</label>
+                                </div>
+                                <div class="col9">
+                                    <select id="District" name="District">
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row" id="DivisionBox">
+                                <div class="col3">
+                                    <label for="user role">Div sec</label>
+                                </div>
+                                <div class="col9">
+                                    <select id="Division" name="Division">
+                                        <option value="null">Select Division</option>   
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row" id="GndivisionBox">
+                                <div class="col3">
+                                    <label for="user role">GN div</label>
+                                </div>
+                                <div class="col9">
+                                    <select id="Gndivision" name="Gndivision">
+                                        <option value="null">Select GN Division</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row " style="text-align:right;justify-content: right;">
+                                <input type="reset" style="background-color: red;" value="Cancel">
+                                <input type="submit" style="background-color: darkblue;margin-top:0px" value="Submit">
+                            </div>
+                        </form>
                 </div>
+            </div>
+        </div>
+        <div id="alertBox">
+        </div>
     </section>
     <script>
         var thisPage = "#ResponsiblePerson";
+        document.getElementById("NIC").addEventListener("keyup",function(e){
+            var nicNum = document.getElementById("NIC").value;
+            var cnic_no_regex = new RegExp('^[0-9+]{9}[vV|xX]$');
+            var new_cnic_no_regex = new RegExp('^[0-9+]{12}$');
+            if (nicNum.length == 10 && cnic_no_regex.test(nicNum)) {
+                $("#nicCheck").html("");
+            } else if (nicNum.length == 12 && new_cnic_no_regex.test(nicNum)) {
+                $("#nicCheck").html("");
+            } else {
+                $("#nicCheck").html("<lable style='color:red'>Please input valid NIC number.</lable>");
+            }
+        });
         $(document).ready(function() {
             $("#Dashboard,#Alerts,#Messages,#Incidents,#IncidentReporting,#Compensation,#Donation,#ResponsiblePerson").each(function() {
                 if ($(this).hasClass('active')){
@@ -104,14 +156,157 @@
                 }
                 $(thisPage).addClass("active");
             });
-
+            var distOptions = "<option value=''>Select District</option>";
+            $.getJSON('<?php echo HOST; ?>public/assets/json/data.json',function(result){
+                $.each(result,function(i,district){
+                    distOptions += "<option value='"+district.dsId+"'>"+district.name+"</option>";
+                });
+                 $('#District').html(distOptions);
+            })
+            $('#DistrictBox').hide();
+            $('#DivisionBox').hide();
+            $('#GndivisionBox').hide();
         });
+        function validate_nic(nic) {
+            var cnic_no_regex = new RegExp('^[0-9+]{9}[vV|xX]$');
+            var new_cnic_no_regex = new RegExp('^[0-9+]{12}$');
+            if (nic.length == 10 && cnic_no_regex.test(nic)) {
+                $("#nicCheck").html("");
+                return true;
+            } else if (nic.length == 12 && new_cnic_no_regex.test(nic)) {
+                $("#nicCheck").html("");
+                return true;
+            } else {
+                $("#nicCheck").html("<lable style='color:red'>Please input valid NIC number.</lable>");
+                return false;
+            }
+        }
+        function isNumber(e) {
+            e = (e) ? e : window.event;
+            var charCode = (e.which) ? e.which : e.keyCode;
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+            }
+            return true;
+        }
+        $('#District').change(function(){
+            var val = $(this).val();
+            var divOptions = "<option value=''>Select Division</option>";
+            $.getJSON('<?php echo HOST; ?>public/assets/json/data.json',function(result){
+                $.each(result,function(i,district){
+                    if(district.dsId == val){
+                        $.each(district.division,function(j,division){
+                           divOptions += "<option value='"+division.dvId+"'>"+division.name+"</option>";  
+                        })
+                    }
+                });
+                $('#Division').html(divOptions);
+            })
+        })
+        $("#add").submit(function(e) {
+                e.preventDefault();
+                var str = [];
+                var formElement = document.querySelector("#add");
+                var formData = new FormData(formElement);
+                var object = {};
+                formData.forEach(function(value, key){
+                    object[key] = value;
+                }); 
+                var json = JSON.stringify(object);
+                console.log(json);
+                $("#add").trigger("reset");
+                $.ajax({
+					type: "POST",
+					url: "<?php echo API; ?>user",
+					data: json,
+                    headers: {'HTTP_APIKEY':'<?php echo $_SESSION['key'] ?>'},
+					cache: false,
+					success: function(result) {
+                        console.log(result);
+                        if(result.code==806){
+                            alertGen("Record Added Successfully!",1);
+                        }else{
+                            alertGen("Unable to handle request.",2);
+                        }
+					},
+					error: function(err) {
+						alertGen("Unable to handle request.",2);
+					}
+				}); 
+            });
+        $('#Division').change(function(){
+            var div = $(this).val();
+            var dist = $('#District').val();
+            var gnOptions = "<option value=''>Select GNDivision</option>";
+            $.getJSON('<?php echo HOST; ?>public/assets/json/data.json',function(result){
+                $.each(result,function(i,district){
+                    if(district.dsId == dist){
+                        $.each(district.division,function(j,division){
+                           if(division.dvId == div){
+                               $.each(division.gnArea, function(k,gnArea){
+                                gnOptions += "<option value='"+gnArea.gndvId+"'>"+gnArea.name+"</option>"; 
+                               })
+                           }
+                        })
+                    }
+                });
+                $('#Gndivision').html(gnOptions);
+            })
+        })
 
         let sidebar = document.querySelector(".sidebar");
         let sidebarBtn = document.querySelector(".sidebarBtn");
         sidebarBtn.onclick = function() {
             sidebar.classList.toggle("active");
         }
+        function alertGen($messege,$type){
+            if ($type == 1){
+                $("#alertBox").html("  <div class='alert success-alert'><h3>"+$messege+"</h3><a id='closeMessege' class='closeMessege'>&times;</a></div>");
+                setTimeout(function() { 
+                    $(".alert").fadeOut(100)
+                    $("#alertBox").html("");
+                }, 4000);
+            }else if($type == 2){
+                $("#alertBox").html("  <div class='alert warning-alert'><h3>"+$messege+"</h3><a id='closeMessege' class='closeMessege'>&times;</a></div>");
+                setTimeout(function() { 
+                    $(".alert").fadeOut(100)
+                    $("#alertBox").html("");
+                }, 4000);
+            }else{
+                $("#alertBox").html("  <div class='alert danger-alert'><h3>"+$messege+"</h3><a id='closeMessege' class='closeMessege'>&times;</a></div>");
+                setTimeout(function() { 
+                    $(".alert").fadeOut(100)
+                    $("#alertBox").html("");
+                }, 4000);
+            }
+        }
+        $(function() {
+            $('#user_role').change(function(){
+                console.log($(this).val());
+                var value = $(this).val();
+                if(value==0){
+                    $('#DistrictBox').hide();
+                    $('#DivisionBox').hide();
+                    $('#GndivisionBox').hide();
+                }else if(value==1){
+                    $('#DistrictBox').show();
+                    $('#DivisionBox').show();
+                    $('#GndivisionBox').show();
+                }else if(value==4){
+                    $('#DistrictBox').show();
+                    $('#DivisionBox').show();
+                    $('#GndivisionBox').hide();
+                }else if(value==3){
+                    $('#DistrictBox').show();
+                    $('#DivisionBox').hide();
+                    $('#GndivisionBox').hide();
+                }else if(value==6){
+                    $('#DistrictBox').show();
+                    $('#DivisionBox').show();
+                    $('#GndivisionBox').hide();
+                }
+            });
+        });
     </script>
 </body>
 </html>

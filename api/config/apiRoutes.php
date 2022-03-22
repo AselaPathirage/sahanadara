7d624eeca9c003e7c9afcf3fcfb7e4995d511108
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-03-22 22:59:03 +0530

    aid request - handle by notice

--- api/config/apiRoutes.php
+++ api/config/apiRoutes.php
@@ -45,6 +45,7 @@ Route::POST("renew", array("Employee@renew"));
 Route::POST("rewoke", array("Employee@rewoke"));
 Route::POST("user", array("Admin@register", "DivisionalSecretariat@register", "DisasterOfficer@register"));
 Route::POST("notice", array("InventoryManager@addNotice", "DisasterOfficer@addNotice"));
+Route::POST("notice/{safeHouseId}", array("InventoryManager@addAidNotice"));
 
 Route::DELETE("notice/{id}", array("InventoryManager@deleteNotice", "DisasterOfficer@deleteNotice"));
 

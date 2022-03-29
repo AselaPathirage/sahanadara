714588537bb38d1771360176f676503837020219
AselaPathirage <aselapathirage@gmail.com>
2022-03-29 22:52:38 +0530

    ui changes

--- public/Views/GramaNiladari/Report/IncidentReport.php
+++ public/Views/GramaNiladari/Report/IncidentReport.php
@@ -48,11 +48,11 @@
                             </div>
                             <div class="col4">
                                 <label for="crusttype">From</label>
-                                <input type="date" id="from" name="from">
+                                <input type="date" id="from" name="from" max="<?php echo date("Y-m-d"); ?>">
                             </div>
                             <div class="col4">
                                 <label for="crusttype">To</label>
-                                <input type="date" id="to" name="to">
+                                <input type="date" id="to" name="to" max="<?php echo date("Y-m-d"); ?>">
                             </div>
                         </div>
 
@@ -77,7 +77,11 @@
                 }
                 $(thisPage).addClass("active");
             });
-
+            $("#from").on("change", function() {
+                var from = $("#from").val();
+                var input = document.getElementById("to");
+                input.setAttribute("min", from);
+            });
             getDisasterType();
 
         });
